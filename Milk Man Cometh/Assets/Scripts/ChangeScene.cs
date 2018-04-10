5ef80f7fa63f05531bdf9bddeb66f5cf9a61c4d6
James Pierson <james.scott.pierson@gmail.com>
2018-04-09 20:16:15 -0400

    Added Box Fort
    Adjusted floor of box fort. Added right wall. Changed layering.

---
+++ Milk Man Cometh/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+
+
+    public string sceneName;
+    public float xPosition;
+    public float yPosition;
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.tag == "Player")
+        {
+            other.transform.position = new Vector3(xPosition, yPosition, 0);
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
